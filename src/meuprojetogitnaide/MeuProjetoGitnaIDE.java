06627796e2011eea207c5a7448c0caf5a256fb3a
20181210031 <20181210031@pc-129392.passofundo.ifsul.local>
2019-10-18 19:48:10 -0300

    nova linha add

--- src/meuprojetogitnaide/MeuProjetoGitnaIDE.java
+++ src/meuprojetogitnaide/MeuProjetoGitnaIDE.java
@@ -16,6 +16,7 @@ public class MeuProjetoGitnaIDE {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Seja bem-vindo ao git no Netbeans");
     }
     
 }
